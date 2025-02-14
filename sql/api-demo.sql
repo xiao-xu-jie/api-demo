4d637ba2904be33b1dba124ae3ae9b50aba431cc
xujie <3285503539@qq.com>
2025-02-14 11:54:02 +0800

    docs(sql): sql数据添加测试数据

--- sql/api-demo.sql
+++ sql/api-demo.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80036
  File Encoding         : 65001
 
- Date: 14/02/2025 01:25:59
+ Date: 14/02/2025 11:53:09
 */
 
 SET NAMES utf8mb4;
@@ -24,8 +24,8 @@ DROP TABLE IF EXISTS `areas`;
 CREATE TABLE `areas`  (
   `id` bigint NOT NULL COMMENT '区域住建',
   `country_code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '国家代码',
-  `province` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '省名称',
-  `city` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '城市名称',
+  `province` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '省名称',
+  `city` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '城市名称',
   `district` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '区（可选）',
   `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -39,6 +39,8 @@ CREATE TABLE `areas`  (
 -- ----------------------------
 INSERT INTO `areas` VALUES (1, 'CN', '四川省', '成都市', NULL, '2025-02-13 21:07:25', '2025-02-13 21:07:01', 0);
 INSERT INTO `areas` VALUES (2, 'CN', '四川省', '广安市', NULL, '2025-02-13 22:01:56', '2025-02-13 22:01:56', 0);
+INSERT INTO `areas` VALUES (3, 'CN', '广东省', NULL, NULL, '2025-02-14 11:44:34', '2025-02-14 11:44:36', 0);
+INSERT INTO `areas` VALUES (4, 'CN', '浙江省', '杭州市', NULL, '2025-02-14 11:45:10', '2025-02-14 11:45:12', 0);
 
 -- ----------------------------
 -- Table structure for user_area
@@ -54,5 +56,7 @@ CREATE TABLE `user_area`  (
 -- ----------------------------
 INSERT INTO `user_area` VALUES (1, 1);
 INSERT INTO `user_area` VALUES (1, 2);
+INSERT INTO `user_area` VALUES (1, 3);
+INSERT INTO `user_area` VALUES (1, 4);
 
 SET FOREIGN_KEY_CHECKS = 1;
